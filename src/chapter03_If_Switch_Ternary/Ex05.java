5588b0d41ec2f661775f9948beecac014e4c821f
Ram-Yas <ramazanyas@gmail.com>
2022-04-04 11:33:16 +0200

    04/04/2022h

--- src/chapter03_If_Switch_Ternary/Ex05.java
+++ src/chapter03_If_Switch_Ternary/Ex05.java
@@ -1,4 +1,12 @@
 package chapter03_If_Switch_Ternary;
 
 public class Ex05 {
+    /*
+    Bir aydaki gün sayısını hesaplayan bir Java programı yazınız.
+    INPUT:
+    Ay Numarasi:2
+    Yil : 2016
+    OUTPUT : Subat 2016 29 Gundur.
+     */
+
 }
