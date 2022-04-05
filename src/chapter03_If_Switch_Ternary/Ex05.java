d31d82280052c155dd19e6e3b322d89d28c42129
Ram-Yas <ramazanyas@gmail.com>
2022-04-05 20:01:05 +0200

    05/04/2022

--- src/chapter03_If_Switch_Ternary/Ex05.java
+++ src/chapter03_If_Switch_Ternary/Ex05.java
@@ -1,5 +1,7 @@
 package chapter03_If_Switch_Ternary;
 
+import java.util.Scanner;
+
 public class Ex05 {
     /*
     Bir aydaki gün sayısını hesaplayan bir Java programı yazınız.
@@ -8,5 +10,48 @@ public class Ex05 {
     Yil : 2016
     OUTPUT : Subat 2016 29 Gundur.
      */
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ay numarasini girin : ");
+        int secim = scan.nextInt();
+        System.out.print("Yil numarasini girin : ");
+        int yil = scan.nextInt();
+
+
+        switch(secim){
+
+            case 2 : System.out.print("Subat "+yil+ " 29 Gundur" );
+                break;
+            case 1 : System.out.print("Ocak "+yil+ " 31 Gundur" );
+                break;
+            case 3: System.out.print("Mart "+yil+ " 31 Gundur" );
+                break;
+            case 4 : System.out.print("Nisan "+yil+ " 30 Gundur" );
+                break;
+            case 5 : System.out.print("Mayis "+yil+ " 31 Gundur" );
+                break;
+            case 6 : System.out.print("Haziran "+yil+ " 30 Gundur" );
+                break;
+            case 7 : System.out.print("Temmuz "+yil+ " 31 Gundur" );
+                break;
+            case 8 : System.out.print("Agustos "+yil+ " 31 Gundur" );
+                break;
+            case 9 : System.out.print("Eylul "+yil+ " 30 Gundur" );
+                break;
+            case 10 : System.out.print("Ekim "+yil+ " 31 Gundur" );
+                break;
+            case 11 : System.out.print("Kasim "+yil+ " 30 Gundur" );
+                break;
+            case 12 : System.out.print("Aralik "+yil+ " 31 Gundur" );
+                break;
+
+
+
+
+        }
+
+    }
 
 }
---
+++ src/chapter04/Eng01.java
@@ -0,0 +1,48 @@
+package chapter04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Eng01 {
+    /*
+    Write a Java program to get a String from user as input and find the maximum occurring character in that string. (Ignore case sensitivity)
+
+input :
+
+Learning java is easy
+
+output:
+
+maximum occurring character is : a
+     */
+
+    public static void main(String[] args) {
+
+        String str="Learningjavaiseasysss";
+
+        String arr[]=str.split("");
+
+        String maxHarf="";
+        int count=0;
+        int maxcount=0;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i+1; j <arr.length ; j++) {
+
+                if (arr[i].equals(arr[j])){
+
+                    count++;
+                }
+
+            }
+            if (count>maxcount){
+                maxcount=count;
+                maxHarf=arr[i];
+            }
+            count=0;
+        }
+        System.out.println("maxHarf = " + maxHarf);
+
+            }
+
+    }
+
