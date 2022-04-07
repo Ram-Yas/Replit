d98484f270b336bdcd201532b8e5f5e199ca0cd1
Ram-Yas <ramazanyas@gmail.com>
2022-04-08 00:21:12 +0200

    087/04/2022

--- src/Eng_Arrays/Ex08.java
+++ src/Eng_Arrays/Ex08.java
@@ -18,12 +18,12 @@ public class Ex08 {
         int []  Input2={5,6};
 
         //System.out.println(mergeArrayMembers(Input1, Input2));
-        mergeArrayMembers(Input1, Input2);
+        System.out.println(Arrays.toString(mergeArrayMembers(Input1, Input2)));
 
 
     }
 
-    private static void mergeArrayMembers(int[] input1, int[] input2) {
+    private static int[] mergeArrayMembers(int[] input1, int[] input2) {
         int newArray [] = new int[input1.length+input2.length];
 
         for (int i = 0; i < input1.length; i++) {
@@ -33,7 +33,7 @@ public class Ex08 {
         for (int i = 0; i < input2.length; i++) {
             newArray [input1.length+i] =input2[i];
         }
-        System.out.println(Arrays.toString(newArray));
-        //return newArray[];
+
+        return newArray;
     }
 }
