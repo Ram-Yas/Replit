753885b72088aea13a68df542242e8dee663078a
Ram-Yas <ramazanyas@gmail.com>
2022-04-13 23:16:18 +0200

    13/04/2022

--- src/Eng_Constructor/Ex01.java
+++ src/Eng_Constructor/Ex01.java
@@ -1,4 +1,6 @@
 package Eng_Constructor;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Ex01 {
     /*
@@ -13,4 +15,101 @@ OK  3- Ask to users if user want to continue game or not OK
        then add letter to word and run step 2
 OK      ii) If player does not want to continue then print "Game Finished" and print points and winner
      */
-}
+
+
+        static int player1_Puan;
+        static int player2_Puan;
+        static String word;
+        static String harf;
+        static String  tercih;
+        static int tercihHarf1;
+        static int tercihHarf2;
+        static int tercihdevam1;
+        static int tercihdevam2;
+        static Scanner scan = new Scanner(System.in);
+
+        public static void main(String[] args) {
+
+            System.out.println("oyuna hosgeldiniz...");
+            System.out.println("1. player bir kelime girsin...");
+            word = scan.nextLine();
+            System.out.println("ikinci player: " + word + " kelimesi kabul ise E degilse H giriniz");
+            tercih = scan.nextLine().toUpperCase(Locale.ROOT);
+            switch (tercih) {
+                case "E":
+                    puanekleme1oyuncu();
+                    break;
+                case "H":
+                    sonuc();
+                    break;}
+
+            ikincioyuncuHarfgirmetodu();
+        }
+
+
+
+        private static void sonuc() {
+            if (player1_Puan > player2_Puan) {
+                System.out.println("Invalid word,end the game");
+                System.out.println(player1_Puan + " puan ile player1 kazandi");
+            } else if (player1_Puan < player2_Puan) {
+                System.out.println("Invalid word,end the game");
+                System.out.println(player2_Puan + " puan ile player2 kazandi");
+            } else if (player1_Puan == player2_Puan) {
+                System.out.println("Invalid word,end the game");
+                System.out.println(" esit puan berabere");
+            }
+
+        }
+
+
+        private static void puanekleme1oyuncu() {
+            player1_Puan += (word.length());
+        }
+
+
+        private static void puanekleme2oyuncu() {
+            player2_Puan += (word.length());
+        }
+
+
+        private static void birincioyuncuHarfgirmetodu() {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("birinci oyuncu gecerli bir harf giriniz");
+            harf = scan.nextLine();
+            System.out.println(harf + " harfi kelimenin basina gelmesi icin 1 && sonu icin 2 giriniz");
+            tercihHarf1 = scan.nextInt();
+            String s = tercihHarf1 == 2 ? (word = word.concat(harf)) : (word = harf.concat(word));
+
+            System.out.println("ikinci oyuncu==> " + word + " kelimesi kabul ise 1 degilse 2 giriniz");
+            tercihdevam2=scan.nextInt();
+            if (tercihdevam2 == 1) {
+                puanekleme1oyuncu();
+                ikincioyuncuHarfgirmetodu();
+
+            } else if (tercihdevam2 == 2) {
+                sonuc();
+            }
+        }
+
+        private static void ikincioyuncuHarfgirmetodu() {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("ikinci oyucu gecerli bir harf giriniz: ");
+            harf = scan.nextLine();
+            System.out.println(harf + " harfi kelimenin basina gelmesi icin 1 && sonu icin 2 giriniz: ");
+            tercihHarf2 = scan.nextInt();
+            String s = tercihHarf2 == 2 ? (word = word.concat(harf)) : (word = harf.concat(word));
+
+            System.out.println("birinci oyuncu==> " + word + " kelimesi kabul ise 1 degilse 2 giriniz: ");
+            tercihdevam1=scan.nextInt();
+            if (tercihdevam1 == 1) {
+                puanekleme2oyuncu();
+                birincioyuncuHarfgirmetodu();
+
+            } else if (tercihdevam1== 2) {
+                sonuc();
+            }}
+
+
+    }
+
--- src/Eng_Constructor/Ex02.java
+++ src/Eng_Constructor/Ex02.java
@@ -1,42 +1,59 @@
 package Eng_Constructor;
 
+
+import java.util.Scanner;
+
 public class Ex02 {
+    static String studentSurname;
+    static String studentName;
 
     /*
-    Create a student's variables in the main method and
-    use this assign variables create a method named Changename,
-    student as parameter get the information and change all the information in the method
-    and add the new information have it print The line where we call the Rename method in the Main method is immediately
-    then reprint student information Create a method named changesurname,
-    this method is "lastname" as parameter get the variable.
-    In the method, ask the user to enter a new last name and change the old last name (i.e. can).
+     1- Create a student's variables in the main method and use this assign variables
+     2- create a method named Changename, student as parameter
+     3- get the information and change all the information in the method and add the new information
+     4- have it print
+     5- The line where we call the Rename method in the Main method is immediately then reprint student information
+     6- Create a method named changesurname, this method is "lastname" as parameter get the variable. In the method, ask the user to enter a new last name and
+     7- change the old last name (i.e. can).
      */
-
     public static void main(String[] args) {
 
-        String studentSurname;
-        String studentName ;
+        String studentSurname = "";
+        String studentName = "";
 
-        studentSurname = "bitmedi";
-        studentName = "Ogrencilik";
-       Ex02 student = new Ex02("ali", "can");
-
+        Ex02 student1 = new Ex02("ali", "can");
+        System.out.println("student1 : " + student1);
+        changeName(student1.studentName);
+        System.out.println(student1);
+        changeSurname(student1.studentSurname);
+        System.out.println(student1);
     }
 
-    private static Object changeName(String studentName, String studentSurname) {
-        studentName = "veli";
-        studentSurname = "gol";
-        return studentName;
+    private static void changeSurname(String studentSurname) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("yeni soyismi giriniz: ");
+        String yeniSoyisim = scan.next();
+        studentSurname = yeniSoyisim;
     }
 
-
-    public Ex02(String studentName, String studentSurname) {
+    private static void changeName(String student1) {
+        studentName = "Ahmet";
+        studentSurname = "yilmaz";
     }
 
+    public Ex02(String studentSurname, String studentName) {
+        this.studentSurname = studentSurname;
+        this.studentName = studentName;
+    }
 
+    public Ex02() {
+    }
 
-
-
-
-
+    @Override
+    public String toString() {
+        return
+                "studentSurname='" + studentSurname + '\'' +
+                        ", studentName='" + studentName + '\'' +
+                        '}';
+    }
 }
